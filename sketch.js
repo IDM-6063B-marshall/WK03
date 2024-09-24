7c5ee97d2ea9d5bd27c75839eace6f0f2c7791e0
Jubilee Marshall <jam1703@nyu.edu>
2024-09-23 22:47:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-function star(x,y,w, c){ 
-  let r = w / 2; 
+function star(x, y, w, c) {
+  let r = w / 2;
   push();
   translate(x, y);
   line(-c, -c, 0, -r);
@@ -17,14 +17,27 @@ function setup() {
   background(300, 120, 500);
   strokeWeight(3);
   //star (100, 100, 50, 5)
- // star (200, 200, 50, 5)
- // star (300, 100, 50, 5)
+  // star (200, 200, 50, 5)
+  // star (300, 100, 50, 5)
   //star (200, 200, 50, 5)
 
-for (let x = 50; x < width ; x += 100)
-  star (x, 50, 80, 10)
-  
-}
+  // drawing a row of stars
 
+  for (let x = 50; x < width; x += 50) star(x, 50, 80, 10);
+
+  // drawing a column of stars
+
+  for (let y = 50; y < height; y += 50) {
+    star(50, y, 80, 10);
+  }
+
+  // draw a column of rows
+  for (let y = 50; y < height; y += 70) {
+    // draw a row of stars
+    for (let x = 50; x < width; x += 100) {
+      star(x, y, 100, 5);
+    }
+  }
+}
 
 function draw() {}
